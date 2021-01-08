3ad8bba437d84fa1423d25236ece9f22d36dd876
Alex <moodspace@outlook.com>
2021-01-08 21:37:29 +0800

    Update lean.js

--- src/generators/lean.js
+++ src/generators/lean.js
@@ -10,6 +10,9 @@ module.exports = {
 		var generateEntry = function (ip, domain) {
 			const prefix = 16
 			const ipRange = ip2Range(ip, 16)
+			if (ip === "127.0.0.1" || ipRange.match(/^10\./) || ipRange.match(/^192\.168\./) || ipRange.match(/^172\.((1[6-9])|(2[0-9])|(3[0-1]))\./)) {
+				return ''
+			}
 			if (dict[ipRange]) {
 				return ''
 			}
